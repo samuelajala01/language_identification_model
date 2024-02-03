030bf2f45c027e33a6a2bfc9a423e1678abb725b
Samuel <samuelajala01@gmail.com>
2024-02-03 13:50:52 -0800

    chore: remove datasets,linear_model

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn import datasets, linear_model, model_selection
+from sklearn import model_selection
 # from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
